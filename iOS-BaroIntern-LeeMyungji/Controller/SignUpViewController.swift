73643ef3a9ea206a2ece2b884cb3a65f3e6dfc0d
zzangmmz <leemj036@gmail.com>
2025-03-15 14:38:11 +0900

    [feat] UI 컴포넌트 정의

---
+++ iOS-BaroIntern-LeeMyungji/Controller/SignUpViewController.swift
@@ -0,0 +1,154 @@
+//
+//  SignUpViewController.swift
+//  iOS-BaroIntern-LeeMyungji
+//
+//  Created by 이명지 on 3/14/25.
+//
+
+import UIKit
+
+final class SignUpViewController: UIViewController {
+    // MARK: - UI Components
+    // ID
+    private let idContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private let idLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이메일"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 18)
+        return label
+    }()
+    
+    private let idTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.backgroundColor = .clear
+        textField.font = .systemFont(ofSize: 18)
+        return textField
+    }()
+    
+    private let idValidationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .red
+        label.font = .systemFont(ofSize: 11)
+        return label
+    }()
+    
+    // password
+    private let passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private let passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "비밀번호"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 18)
+        return label
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.backgroundColor = .clear
+        textField.font = .systemFont(ofSize: 18)
+        return textField
+    }()
+    
+    private let togglePasswordButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .clear
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private let passwordValidationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .red
+        label.font = .systemFont(ofSize: 11)
+        return label
+    }()
+    
+    // password-check
+    private let passwordCheckContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private let passwordCheckLabel: UILabel = {
+        let label = UILabel()
+        label.text = "비밀번호 확인"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 18)
+        return label
+    }()
+    
+    private let passwordCheckTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.backgroundColor = .clear
+        textField.font = .systemFont(ofSize: 18)
+        return textField
+    }()
+    
+    private let togglePasswordCheckButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .clear
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private let passwordCheckValidationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .red
+        label.font = .systemFont(ofSize: 11)
+        return label
+    }()
+    
+    // nickname
+    private let nicknameContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private let nicknameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "닉네임"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 18)
+        return label
+    }()
+    
+    private let nicknameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.backgroundColor = .clear
+        textField.font = .systemFont(ofSize: 18)
+        return textField
+    }()
+    
+    // sign-up button
+    private let signUpButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 8
+        button.setTitle("회원가입", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18)
+        return button
+    }()
+}
