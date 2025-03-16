5deebe620494d31a8d5cf94422b62b6367cb4e15
zzangmmz <leemj036@gmail.com>
2025-03-16 17:34:48 +0900

    [feat] User 모델 생성

---
+++ iOS-BaroIntern-LeeMyungji/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  iOS-BaroIntern-LeeMyungji
+//
+//  Created by 이명지 on 3/16/25.
+//
+
+struct User {
+    let id: String
+    let password: String
+    let nickname: String
+}
