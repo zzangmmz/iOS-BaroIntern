82bba2483ced463cc229e5ff1ce5f8da8e5ef94c
zzangmmz <leemj036@gmail.com>
2025-03-16 11:01:54 +0900

    [chore] 빈 메서드 삭제

--- iOS-BaroIntern-LeeMyungji/View/InputTitleLabel.swift
+++ iOS-BaroIntern-LeeMyungji/View/InputTitleLabel.swift
@@ -19,7 +19,4 @@ final class InputTitleLabel: UILabel {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func setupUI() {
-    }
 }
