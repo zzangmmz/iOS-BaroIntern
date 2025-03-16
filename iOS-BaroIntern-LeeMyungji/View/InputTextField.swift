087ef6714295b4b8f3438c43892c994801109843
zzangmmz <leemj036@gmail.com>
2025-03-16 14:18:30 +0900

    [refactor] 파라미터로 keyboardType 받도록 수정, 텍스트필드 속성 수정

--- iOS-BaroIntern-LeeMyungji/View/InputTextField.swift
+++ iOS-BaroIntern-LeeMyungji/View/InputTextField.swift
@@ -8,13 +8,17 @@
 import UIKit
 
 final class InputTextField: UITextField {
-    init(ofSize: CGFloat = 18, isSecured: Bool = false) {
+    init(ofSize: CGFloat = 18, isSecured: Bool = false, keyboardType: UIKeyboardType = .default) {
         super.init(frame: .zero)
         
         self.borderStyle = .none
         self.backgroundColor = .clear
         self.font = .systemFont(ofSize: ofSize)
         self.isSecureTextEntry = isSecured
+        self.autocapitalizationType = .none
+        self.autocorrectionType = .no
+        self.spellCheckingType = .no
+        self.keyboardType = keyboardType
     }
     
     required init?(coder: NSCoder) {
